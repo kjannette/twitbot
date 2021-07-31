1c03496efb246b0283113013e6e0df1bb31b023e
kjannette <sj@sjdev.co>
2021-07-31 15:48:20 -0500

    Frontend fun

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,26 @@ import './App.css';
 
 const App = () => {
 
-  const [tweets, setTweets ] = useState([])
+  const [state, setState ] = useState([])
+
+  state.forEach(item => {
+    console.log("this is a tweet", item)
+  })
 
   let searchTerm; 
 
   const outputTweets = (data) => {
-    console.log(data)
-    console.log('state tweets', tweets)
+    console.log('')
   }
 
   const getTweets = () => {
     console.log('getAllTweets HAS FIRED')
       const url = '/tweets/' + searchTerm;
-      fetch(url).then(function (response) {
+      fetch(url).then((response) => {
           return response.json()
-      }).then(function (data) {
-          outputTweets(data.statuses)
-          setTweets(data.statuses)
-      }).catch(function (error) {
+      }).then((data) => {
+          setState(data.statuses)
+      }).catch((error) => {
           console.log(JSON.stringify(error));
       })
   }
@@ -30,11 +32,27 @@ const App = () => {
       <div>This is client App.js</div>
       <input type="text" value={searchTerm}></input>
       <button type="submit" onClick={getTweets}></button>
-      {tweets.map((tweet, i) => {
-        <div id={i}>{tweet.text}</div>
-      })}
+      {
+        state.map((item, index) => {
+          <div key={index}>{item.text}</div>
+        })
+      }
     </div>
   );
 }
 
 export default App;
+
+/*
+
+      {state.map(item, i => {
+        <div id={i}>{item.text}</div>
+      }
+      )}
+
+      (function () {
+          fetch('http://localhost:3000/users.json')
+          .then(response => response.json())
+          .then(data => setState(data));
+        })();
+*/
