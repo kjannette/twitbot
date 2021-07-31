3b015c4e8a64cba2abdc846578f2aa416a7ad932
kjannette <sj@sjdev.co>
2021-07-31 14:48:56 -0500

    Additional client functionality

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,13 @@ const App = () => {
 
   let search; 
 
-  const outputTweets = () => {
-    console.log('something')
+  const outputTweets = (data) => {
+    console.log(data)
   }
 
   const getAllTweets = () => {
     console.log('getAllTweets HAS FIRED')
-      const url = '/tweets/' + search.value;
+      const url = '/tweets/' + 'sports'//search.value;
       fetch(url).then(function (response) {
           return response.json()
       }).then(function (data) {
@@ -23,6 +23,8 @@ const App = () => {
   return (
     <div className="App">
       <div>This is client App.js</div>
+      <input type="text"></input>
+      <button onClick={getAllTweets}></button>
     </div>
   );
 }
