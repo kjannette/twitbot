e00e9f605fa254704db4d548543d9f0aec7854c8
Ken Jannette <kennethjannette@kj.local>
2021-07-31 09:59:33 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const twit = require('twit');
+ 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+    extended: false
+  }));
+
+app.get('/tweets/:search', function(req,res){
+    console.log(req.params.search);
+    res.json({'status':'Success'})
+})
+ 
+app.listen(3000)
+
+console.log("RESTART")
