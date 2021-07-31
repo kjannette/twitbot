0282968bda2079509e25fe92f981a7a45ae92920
kjannette <sj@sjdev.co>
2021-07-31 16:26:56 -0500

    Input working for API search term, crdue UI to display returned tweets

--- client/src/App.js
+++ client/src/App.js
@@ -4,37 +4,35 @@ import './App.css';
 const App = () => {
 
   const [state, setState ] = useState([])
-
+  const [searchTerm, setSearchTerm] = useState('')
+  /*
   state.forEach(item => {
     console.log("this is a tweet", item)
   })
+*/
 
-  let searchTerm; 
-
-  const outputTweets = (data) => {
-    console.log('')
-  }
+  console.log(state)
 
   const getTweets = () => {
-    console.log('getAllTweets HAS FIRED')
-      const url = '/tweets/' + searchTerm;
-      fetch(url).then((response) => {
-          return response.json()
-      }).then((data) => {
-          setState(data.statuses)
-      }).catch((error) => {
-          console.log(JSON.stringify(error));
-      })
+    console.log(searchTerm)
+    const url = '/tweets/' + searchTerm;
+    fetch(url).then(function (response) {
+        return response.json()
+    }).then(function (data) {
+        setState(data.statuses)
+    }).catch(function (error) {
+        console.log(JSON.stringify(error));
+    })
   }
 
   return (
     <div className="App">
       <div>This is client App.js</div>
-      <input type="text" value={searchTerm}></input>
+      <input type="text" onChange={event => setSearchTerm(event.target.value)}></input>
       <button type="submit" onClick={getTweets}></button>
       {
         state.map((item, index) => (
-          <div key={index}>{item.text}</div>
+          <div key={index}>`${item.text}`</div>
         ))
       }
     </div>
@@ -42,3 +40,16 @@ const App = () => {
 }
 
 export default App;
+
+/*
+function getAllTweets() {
+  const url = '/tweets/' + search.value;
+  fetch(url).then(function (response) {
+      return response.json()
+  }).then(function (data) {
+      outputTweets(data.statuses)
+  }).catch(function (error) {
+      console.log(JSON.stringify(error));
+  })
+}
+*/
