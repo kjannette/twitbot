d3c7a57b971235a2e87d02d22c4f93df377d6bf7
kjannette <sj@sjdev.co>
2021-07-31 15:20:57 -0500

    More frontend functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+
+  const [tweets, setTweets ] = useState([])
 
   let searchTerm; 
 
   const outputTweets = (data) => {
     console.log(data)
+    console.log('state tweets', tweets)
   }
 
-  const getAllTweets = () => {
+  const getTweets = () => {
     console.log('getAllTweets HAS FIRED')
       const url = '/tweets/' + searchTerm;
       fetch(url).then(function (response) {
           return response.json()
       }).then(function (data) {
           outputTweets(data.statuses)
+          setTweets(data.statuses)
       }).catch(function (error) {
           console.log(JSON.stringify(error));
       })
@@ -24,7 +29,10 @@ const App = () => {
     <div className="App">
       <div>This is client App.js</div>
       <input type="text" value={searchTerm}></input>
-      <button type="submit" onClick={getAllTweets}></button>
+      <button type="submit" onClick={getTweets}></button>
+      {tweets.map((tweet, i) => {
+        <div id={i}>{tweet.text}</div>
+      })}
     </div>
   );
 }
